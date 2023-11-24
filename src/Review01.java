0e21532be6a4258fdc12456ec317651242a52356
gooseatTOKYO <hiromi.karizumi.0418@gmail.com>
2023-11-25 01:34:55 +0900

    taxメソッド内で小数点切り捨て（整数値）を取るようにした。税抜価格と消費税率を変数とした。

--- src/Review01.java
+++ src/Review01.java
@@ -2,18 +2,20 @@
 public class Review01 {
 
     public static void main(String[] args) {
-        //tax(int price,double rate)の呼び出し　※税抜価格1500円、消費税率10%は固定
-        double result = tax(1500, 10);
+        //tax(int price,double rate)の呼び出し ※税込価格1500円、消費税率10%の場合
+        int price = 1500;
+        int rate = 10;
+        int result = tax(price, rate);
 
         //演算結果の表示
-        System.out.println(1500 + "円の商品の税込価格は"+ (1500 + (int)result) + "円（消費税は" + (int)result + "円）です。");
+        System.out.println(price + "円の商品の税込価格は"+ (rate + result) + "円（消費税は" + result + "円）です。");
     }
 
-    // int型変数1つ、double型を受け取り、乗算結果を表示するtaxメソッド
-    public static double tax(int price,double rate) {
-        rate = rate * 0.01;                           //百分率計算用
+    // int型変数1つ、double型1つを受け取り、乗算結果として消費税額を計算するtaxメソッド
+    public static int tax(int price,double rate) {
+        rate = rate * 0.01;                         //百分率計算用
         double result= price * rate;                //消費税額の計算
-        return result;
+        return (int)result;
     }
 
 }
